7b1a5ec9ab669f6986a6c121e4834a160d20e901
oshiumitsuyoshi <ot104752gmail.com>
2025-02-17 18:41:49 +0900

    Initial Commit

---
+++ marubatu2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  marubatu2
+//
+//  Created by 鴛海剛 on 2025/02/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ marubatu2/marubatu2App.swift
@@ -0,0 +1,17 @@
+//
+//  marubatu2App.swift
+//  marubatu2
+//
+//  Created by 鴛海剛 on 2025/02/17.
+//
+
+import SwiftUI
+
+@main
+struct marubatu2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
